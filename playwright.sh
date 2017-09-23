b93a6079f1cc4ab421ae40dc3d99872362e3faf9
mgregory-carecloud <mgregory@carecloud.com>
2017-09-23 16:44:35 -0400

    INST-2 - Cancelled installation if bash profile not given.

--- playwright.sh
+++ playwright.sh
@@ -5,8 +5,6 @@ playwright() {
   [ -z "$1" ] && echo "You gotta supply an argument bruh"
   export PLAYWRIGHT_BASH_PROFILE="$1"
   case $PLAYWRIGHT_BASH_PROFILE in
-    # Throw an error if they try to install without giving the path to their bash profile
-    # [ -z "$2" ] && echo "You gotta gimme your bash profile bruh"
     # 'install')
     #   # Create file structure
     #   md -pv ~/playwright/plays ~/playwright/lib/classes ~/playwright/lib/modules ~/playwright/shell ~/playwright/.src
