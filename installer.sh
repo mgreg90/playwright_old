b661d606b83309ac11d28b2b756e8bc7d900323f
mgregory-carecloud <mgregory@carecloud.com>
2017-09-23 18:02:15 -0400

    INST-3 - Added sourcing of playwright.sh. Improved Error handling.

--- installer.sh
+++ installer.sh
@@ -1,10 +1,56 @@
 # Welcome to playwright!
 
-# Cancel install if no bash profile given
-[ -z "$2" ] && echo "You gotta gimme your bash profile bruh" && exit
-# Create file structure
-md -pv ~/playwright/plays ~/playwright/lib/classes ~/playwright/lib/modules ~/playwright/shell ~/playwright/.src
-# Copy this repo over
-cp -Rv $(pwd) ~/playwright/.src
-# Blammo! Done installing!
-echo 'Playwright has been installed!'
+function install() {
+  # Cancel install if no bash profile given
+  [ -z "$1" ] && echo "You gotta gimme your bash profile bruh" && return 1
+  
+  # Cancel install if invalid bash profile path is given
+  [ -d "$1" ] && echo "Invalid bash profile path bruh" && return 1
+  
+  # Copy current files to temp path
+  [ -d ~/playwright ] && cp -R ~/playwright ~/playwright_temp
+  
+  # Store bash profile path in variable
+  export PLAYWRIGHT_BASH_PROFILE="$1"
+  
+  # Create file structure
+  mkdir -p ~/playwright/plays ~/playwright/lib/classes ~/playwright/lib/modules ~/playwright/shell ~/playwright/.src
+  echo 'File Structure Created'
+  
+  # Copy this repo over
+  cp -R $(pwd) ~/playwright/.src
+  echo 'Repo Copied Over'
+  
+  # source playwright.sh in bash profile
+  TEST_STRING="source '~/playwright/playwright.sh'"
+  if grep -q "$TEST_STRING" "$PLAYWRIGHT_BASH_PROFILE"
+  then
+    echo 'playwright.sh Already Sourced'
+  else
+    echo "\n\n# Source playwright ruby scripting framework\nsource '~/playwright/playwright.sh'" >> $PLAYWRIGHT_BASH_PROFILE || (echo "Invalid Bash Profile Path!" && return 1)
+    echo 'Sourced playwright.sh'
+  fi
+  
+  # Blammo! Done installing!
+  echo 'Playwright has been installed!'
+  
+}
+
+function uninstall() {
+  if [ -d ~/playwright_temp ]
+  then
+    cp -R ~/playwright_temp ~/playwright
+    rm -rf ~/playwright_temp
+  else
+    rm -rf ~/playwright
+  fi
+  
+  if grep -Fxq "source '~/playwright/playwright.sh'" $PLAYWRIGHT_BASH_PROFILE
+  then
+    sed -i "source '~/playwright/playwright.sh'" $PLAYWRIGHT_BASH_PROFILE
+    sed -i "# Source playwright ruby scripting framework" $PLAYWRIGHT_BASH_PROFILE
+  fi
+  echo "Install failed!"
+}
+
+install "$@" || uninstall
